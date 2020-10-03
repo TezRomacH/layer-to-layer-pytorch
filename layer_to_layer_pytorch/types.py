6564ee491e764ca721b133e95e3f34e2d223add0
Roman Tezikov <tez.romach@gmail.com>
2020-10-03 10:44:49 +0000

    🤗 First commit from Codespace

--- layer_to_layer_pytorch/types.py
+++ layer_to_layer_pytorch/types.py
@@ -1,10 +1,8 @@
-from typing import Callable, List, Union
+from typing import Callable, Union
 
 import torch
 
 Device = Union[str, torch.device]
-
-
 Criterion = Callable[[torch.Tensor, torch.Tensor], torch.Tensor]
 
 __all__ = ["Device", "Criterion"]
